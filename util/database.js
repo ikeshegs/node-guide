4013ead61e613e8bfe0edbce191899815cbea11a
Okoro Ikechukwu <ikeshegs@gmail.com>
2019-08-27 16:20:55 +0100

    setup mongodb connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const dotenv = require('dotenv');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'C00ljoe.', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+dotenv.config();
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient
+    .connect(process.env.DB_URL)
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch(err => console.log(err));
+}
+
+module.exports = mongoConnect;
